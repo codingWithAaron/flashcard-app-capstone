72cd038c87f2deb7dc1ba2edcbdd3a2ab5f3a50e
Aaron Arguelles <129692260+codingWithAaron@users.noreply.github.com>
2023-06-14 10:38:27 -0600

    AddCard functionality complete

--- src/cards/AddCard.js
+++ src/cards/AddCard.js
@@ -1,12 +1,26 @@
 import React from "react";
 import CardForm from "./CardForm";
 import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import { useState, useEffect } from "react";
+import { readDeck } from "../utils/api";
 
 function AddCard(){
     const {deckId} = useParams()
+    const [currentDeck, setCurrentDeck] = useState([])
+    const addHeader = `${currentDeck.name}: Add Card`
+    const initialData = {
+        front: "",
+        back: "",
+        deckId: deckId
+    }
+
+    useEffect(()=>{
+        readDeck(deckId)
+        .then(data => setCurrentDeck(data))
+    },[])
 
     return (
-        <CardForm deckId={deckId} />
+        <CardForm deckId={deckId} addHeader={addHeader} initialData={initialData}/>
     )
 }
 
--- src/cards/CardForm.js
+++ src/cards/CardForm.js
@@ -1,7 +1,79 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import {Link} from "react-router-dom"
+import { readDeck } from "../utils/api";
+import { createCard } from "../utils/api";
 
-function CardForm({deckId}){
+function CardForm({deckId, addHeader, initialData}){
+    const [currentDeck, setCurrentDeck] = useState([])
+    const [formData, setFormData] = useState(initialData)
 
+    function handleChange(event){
+        setFormData({
+            ...formData,
+            [event.target.name]: event.target.value
+        })
+    }
+
+    function handleSubmit(event){
+        event.preventDefault()
+        createCard(deckId, formData)
+        setFormData(initialData)
+    }
+
+    useEffect(()=>{
+        readDeck(deckId)
+        .then(data => setCurrentDeck(data))
+    },[])
+
+    if(currentDeck){
+        return (
+            <>
+                <div className="container">
+                    <nav>
+                        <ol className="breadcrumb">
+                            <li className="breadcrumb-item">
+                                 {" "}
+                                <Link to="/">Home</Link>
+                            </li>
+                            <li className="breadcrumb-item">
+                                 {" "}
+                                <Link to={`/decks/${deckId}`}>{currentDeck.name}</Link>
+                            </li>
+                            <li className="breadcrumb-item">Add Card</li>
+                        </ol>
+                    </nav>
+                    <div>
+                        <h2>{addHeader}</h2>
+                    </div>
+                    <div className="d-flex flex-column">
+                        <form onSubmit={handleSubmit}>
+                            <div>
+                                <label htmlFor="front">Front</label>
+                            </div>
+                            <div>
+                                <textarea className="w-100" id="front" type="text" name="front" placeholder=" Front side of card" value={formData.front} onChange={handleChange}></textarea>
+                            </div>
+                            <div className="mt-2">
+                                <label htmlFor="back">Back</label>
+                            </div>
+                            <div>
+                                <textarea className="w-100" id="back" type="text" name="back" placeholder=" Back side of card" value={formData.back} onChange={handleChange}></textarea>
+                            </div>
+                            <div className=" d-flex flex-row mt-2">
+                                <div className="mr-2">
+                                    <Link to={`/decks/${deckId}`} className="btn btn-secondary">Done</Link>
+                                </div>
+                                <div>
+                                    <button className="btn btn-primary" type="submit">Save</button>
+                                </div>
+                            </div>          
+                        </form>
+                    </div>
+                </div>
+            </>
+        )
+    }
 }
 
 export default CardForm
